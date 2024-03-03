41ea8c1a4c0038e6e6c6f0192b9030f6c027fc62
Whitehacker-Frontend <whitehackerfrontend@gmail.com>
2024-03-03 07:37:10 +0500

    Conquest Watches

---
+++ js/script.js
@@ -0,0 +1,10 @@
+let bars = document.querySelector('.bars')
+bars.style.display = 'none'
+function barsFunction() {
+    if (bars.style.display = ' none'){
+        bars.style.display = 'flex'
+    }else{
+        bars.style.display = 'none'
+    }
+    
+}
